20b41d8a1bfe0060224c55aa60e2c2e4abd4229d
DXTPAxT <taidoyasuovn@gmail.com>
2025-05-22 22:47:07 +0700

    Create login servlet

---
+++ src/java/models/User.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author PC ASUS
+ */
+public class User {
+    private int UserID;
+    private int RoleID;
+    private String Fullname;
+    private String Email;
+    private String PhoneNumber;
+    private String Address;
+    private String Password;
+    private String CreatedAt;
+    private int Status;
+
+    public User(int UserID, int RoleID, String Fullname, String Email, String PhoneNumber, String Address, String Password, String CreatedAt, int Status) {
+        this.UserID = UserID;
+        this.RoleID = RoleID;
+        this.Fullname = Fullname;
+        this.Email = Email;
+        this.PhoneNumber = PhoneNumber;
+        this.Address = Address;
+        this.Password = Password;
+        this.CreatedAt = CreatedAt;
+        this.Status = Status;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public int getRoleID() {
+        return RoleID;
+    }
+
+    public void setRoleID(int RoleID) {
+        this.RoleID = RoleID;
+    }
+
+    public String getFullname() {
+        return Fullname;
+    }
+
+    public void setFullname(String Fullname) {
+        this.Fullname = Fullname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(String CreatedAt) {
+        this.CreatedAt = CreatedAt;
+    }
+
+    public int getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int Status) {
+        this.Status = Status;
+    }
+    
+    
+}
