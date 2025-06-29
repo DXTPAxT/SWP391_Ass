ad802110c9281c973a0acbdb8cf6fc58740b1ce6
KhacQuoc0910 <QuocDKHE180045@fpt.edu.vn>
2025-06-29 23:01:56 +0700

    Fix BuildPC Admin insert

--- src/java/dalAdmin/DBAdminContext.java
+++ src/java/dalAdmin/DBAdminContext.java
@@ -33,7 +33,7 @@ public class DBAdminContext {
             String pass = "123";
 
 
-            String url = "jdbc:sqlserver://laptop-dxt\\SQLEXPRESS:1433;databaseName=ComputerOnlineShop";
+            String url = "jdbc:sqlserver://LAPTOP-8RQSUOPU\\SQLEXPRESS:1433;databaseName=ComputerOnlineShop";
 
 
 
