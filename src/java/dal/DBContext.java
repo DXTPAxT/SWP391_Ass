2cfaac67fcad8510fa776d259464d308a41bd9e0
DXTPAxT <taidoyasuovn@gmail.com>
2025-07-04 10:04:39 +0700

    update checkout UI

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -15,7 +15,7 @@ public class DBContext {
             String user = "sa";
             String pass = "123";
 
-            String url = "jdbc:sqlserver://LAPTOP-63C2NPU0\\SQLEXPRESS:1433;databaseName=ComputerOnlineShop";
+            String url = "jdbc:sqlserver://LAPTOP-dxt\\SQLEXPRESS:1433;databaseName=ComputerOnlineShop";
 
 
 
