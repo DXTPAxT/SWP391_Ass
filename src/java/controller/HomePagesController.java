22065da1d0c4caaf4a6051d9d3d78e6e43be414c
haist0321 <haigamer13062004@gmail.com>
2025-07-25 09:16:02 +0700

    Merge branch 'main' into NotificationAdmin-feature

--- src/java/controller/HomePagesController.java
+++ src/java/controller/HomePagesController.java
@@ -77,13 +77,13 @@ public class HomePagesController extends HttpServlet {
       @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       //processRequest(request, response);
+       processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
+        processRequest(request, response);
         
     }
 
